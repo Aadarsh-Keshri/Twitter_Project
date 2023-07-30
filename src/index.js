70ce3eecab4dc6dcc71d9ee5b925a07fb43ad9d3
Aadarsh-Keshri <120744385+Aadarsh-Keshri@users.noreply.github.com>
2023-07-30 11:58:06 +0530

    Added CRUD for tweets model

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import {connect} from "./config/database.js"
 import Tweet from "./models/tweet.js";
 import HashTag from "./models/hashtag.js";
 import { PORT } from "./config/server-config.js";
+import TweetRepository from "./repositories/tweet-repository.js";
 
 const app=express();
 
@@ -10,15 +11,18 @@ app.listen(PORT,async ()=>{
     console.log(`Started the server at PORT:${PORT}`);
     //mongo db connection establishment
     connect();
-    Tweet.create({
-        content: "Waazzzupppp",
-        likes: 25,
-        noOfRetweets: 5,
-        comment: "Yo Yo Yo!!!"
-    })
+    // Tweet.create({
+    //     content: "Waazzzupppp",
+    //     likes: 25,
+    //     noOfRetweets: 5,
+    //     comment: "Yo Yo Yo!!!"
+    // })
     // HashTag.create({
     //     text: "travel",
     //     tweets: ['64c4d4bcb8124afabdb0eea3']
     // })
+    const tweetRepo=new TweetRepository();
+    let response=await tweetRepo.deleteTweet({"_id": '64c3d136ab24556d8003842e'})
+    console.log(response);
     console.log('Mongo DB connected')
 })
